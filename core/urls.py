aef2ef549312b2d6d285822d0535802ab1f91b80
thisisfaisalhere <thisisfaisalhere@gmail.com>
2021-08-15 09:41:28 +0530

    added api docs

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,18 @@ from django.urls.conf import include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
+    # api docs
+    path('', include_docs_urls(title='Blog API')),
+    path('schema/', get_schema_view(
+        title="Blog API",
+        description="API for the Blog",
+        version="1.0.0"
+    ), name='openapi-schema'),
+
     # admin route
     path('admin/', admin.site.urls),
     # user route
